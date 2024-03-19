28d6c4101db98daef90fc07107af67316ddbdbdf
Adem Hatay <hatayadem5@gmail.com>
2024-03-20 00:56:33 +0300

    feature: jwt passport permissions and guard.

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,42 +1,52 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Post,
-    Put,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Items } from '../items';
 import { Item } from '../item';
+import { AuthGuard } from '@nestjs/passport';
+import { Permissions } from 'src/permissions/permissions.decorator';
+import { PermissionsGuard } from 'src/permissions/permissions.guard';
 
 @Controller('items')
 export class ItemsController {
-    constructor(private readonly itemsService: ItemsService) { }
+  constructor(private readonly itemsService: ItemsService) {}
 
-    @Get()
-    async findAll(): Promise<Items> {
-        return this.itemsService.findAll();
-    }
+  @Get()
+  async findAll(): Promise<Items> {
+    return this.itemsService.findAll();
+  }
 
-    @Get(':id')
-    async find(@Param('id') id: number): Promise<Item> {
-        return this.itemsService.find(id);
-    }
+  @Get(':id')
+  async find(@Param('id') id: number): Promise<Item> {
+    return this.itemsService.find(id);
+  }
 
-    @Post()
-    async create(@Body('item') item: Item): Promise<void> {
-        this.itemsService.create(item);
-    }
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+  @Post()
+  @Permissions('create:items')
+  create(@Body('item') item: Item) {
+    this.itemsService.create(item);
+  }
 
-    @Put()
-    async update(@Body('item') item: Item): Promise<void> {
-        this.itemsService.update(item);
-    }
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+  @Put()
+  @Permissions('update:items')
+  update(@Body('item') item: Item) {
+    this.itemsService.update(item);
+  }
 
-    @Delete(':id')
-    async delete(@Param('id') id: number): Promise<void> {
-        this.itemsService.delete(id);
-    }
+  @UseGuards(AuthGuard('jwt'), PermissionsGuard)
+  @Delete(':id')
+  @Permissions('delete:items')
+  delete(@Param('id') id: number) {
+    this.itemsService.delete(id);
+  }
 }
